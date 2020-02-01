f82913848ca8aad686465e563295901b0c9ab920
bbpumpkin <996344439@qq.com>
2020-02-01 23:46:02 +0800

    Add model and README

--- fake/models.py
+++ fake/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Paper(models.Model):
+    uid = models.CharField(max_length=30)  # test user id
+    mid = models.CharField(max_length=30)  # test model id
+    true_cnt = models.IntegerField()  # true positive count
+    false_cnt = models.IntergerField()  # false positive count
